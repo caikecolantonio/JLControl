5f9764a54e0c1497b922e01abc31a3a505fa221d
caikecolantonio <caikecolantonio@hotmail.com>
2021-05-09 16:32:51 -0300

    update requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from decouple import config
 from pathlib import Path
 import os
-from dj_database_url import parse as dburl
+from dj_database_url import parse as dburl, config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
