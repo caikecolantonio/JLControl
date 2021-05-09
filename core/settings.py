03d3e0414ecfe6004d056dff785466e5cdb5e9ff
caikecolantonio <caikecolantonio@hotmail.com>
2021-05-09 16:25:36 -0300

    update requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from distutils.command.config import config
+from decouple import config
 from pathlib import Path
 import os
 from dj_database_url import parse as dburl
